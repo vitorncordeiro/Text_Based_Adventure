d2a0b0dbb1df9a1667d65c6e05565afcc0afec4f
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-09-05 20:50:32 -0300

    Update Location.java

--- src/Location.java
+++ src/Location.java
@@ -6,14 +6,30 @@ import java.util.Random;
 public class Location {
     private Map<Character, String> nextPlaces;
     private String locationDescription;
+    private String asciiArt;
     private int goldQuantity;
 
-    public Location(String locationDescription, Map<Character, String> nextPlaces){
+    public Location(String locationDescription, Map<Character, String> nextPlaces, String asciiArt){
         this.locationDescription = locationDescription;
         this.nextPlaces = nextPlaces;
+        this.asciiArt = asciiArt;
         Random random = new Random();
         this.goldQuantity = random.nextInt(0, 1000);
 
+    }
+    public String showNextPlaces(){
+        String res = "";
+        for(var p : nextPlaces.entrySet()){
+            res += p.getKey() + ": " + p.getValue() + "\n";
+        }
+        return res;
+    }
+    public String getAsciiArt(){
+        return asciiArt;
+    }
+    @Override
+    public String toString(){
+        return locationDescription;
     }
 
 }
