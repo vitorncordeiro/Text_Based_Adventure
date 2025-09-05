2119b22c8e0802e4a6a7153a0343d17a8a35eec0
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-09-05 20:50:43 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
 package code;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocationData locations = new LocationData();
+        System.out.println("Lets start the adventure");
+        Location currentLocation = locations.getLocations().get("camping");
+        boolean flag = true;
+        while(flag){
+            System.out.println("Current Location: " + currentLocation);
+            System.out.println("Choose an direction:\n" + locations.getLocations().get("camping").showNextPlaces());
+            String direction = scanner.nextLine();
+            if(locations.getLocations().containsKey(direction)){
+                //FAZ UM SWITCH CASE AQUI AGORA COM AS DIREÇÕES
+            }
+        }
 
+
+//        for (var location : locations.getLocations().entrySet()){
+//            System.out.println(location.getValue().getAsciiArt());
+//            System.out.println(location.getValue() + "\nFrom here, you can go to these places:");
+//            System.out.println(location.getValue().showNextPlaces());
+//        }
 
     }
 
