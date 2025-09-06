32038a49df4e8c083bd2388a4f94dea83d69e763
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-09-06 10:45:47 -0300

    Update Location.java

--- src/Location.java
+++ src/Location.java
@@ -4,15 +4,17 @@ import java.util.Map;
 import java.util.Random;
 
 public class Location {
-    private Map<Character, String> nextPlaces;
+    private Map<String, String> nextPlaces;
     private String locationDescription;
+    private String locationName;
     private String asciiArt;
     private int goldQuantity;
 
-    public Location(String locationDescription, Map<Character, String> nextPlaces, String asciiArt){
+    public Location(String locationName, String locationDescription, Map<String, String> nextPlaces, String asciiArt){
         this.locationDescription = locationDescription;
         this.nextPlaces = nextPlaces;
         this.asciiArt = asciiArt;
+        this.locationName = locationName;
         Random random = new Random();
         this.goldQuantity = random.nextInt(0, 1000);
 
@@ -27,9 +29,18 @@ public class Location {
     public String getAsciiArt(){
         return asciiArt;
     }
+    public String getLocationName(){
+        return locationName;
+    }
+    public Map<String, String> getNextPlaces(){
+        return nextPlaces;
+    }
+    public String getLocationDescription(){
+        return locationDescription;
+    }
     @Override
     public String toString(){
-        return locationDescription;
+        return locationName;
     }
 
 }
